9eb89b3a0b9f793308a7c260603b1043dc3dca7e
Ekambaram-Yamuna <yamunaekambaram@gmail.com>
2025-04-30 23:15:43 +0530

    Add files via upload

---
+++ AbstractExample.java
@@ -0,0 +1,16 @@
+package Java;
+
+abstract class AbstractExample {
+	public abstract void add(); // abstract method
+	public void sub() // non-abstract method
+	{
+		System.out.println("sub");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+
+	}
+
+}
