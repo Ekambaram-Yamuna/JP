07bee6ec9d04f62b27e663916148be7f88dd1a5b
Ekambaram-Yamuna <yamunaekambaram@gmail.com>
2025-04-30 23:19:00 +0530

    Add files via upload

---
+++ AdvancedForLoop.java
@@ -0,0 +1,15 @@
+package Java;
+
+public class AdvancedForLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a[]= {1,2,3,4,5}; // only array should create .
+		for(int number:a)
+		{
+			System.out.println(number);
+		}
+
+	}
+
+}
