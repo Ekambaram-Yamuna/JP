1c543a537b4a30781e7b4a11583994769b1fda90
Ekambaram-Yamuna <yamunaekambaram@gmail.com>
2025-04-30 23:21:11 +0530

    Add files via upload

---
+++ assign_operator.java
@@ -0,0 +1,29 @@
+package Java;
+
+public class assign_operator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//create variables
+		int a=5;
+		int var;
+		//assign value using =
+		var=a;
+		System.out.println("var using =:"+var);
+		//assign value using =+
+		
+		var += a; // var =var +a
+		System.out.println("var using +=:"+var);
+		//assign value using =*
+		
+		var *=a; // var =var*a
+		System.out.println("var using *=:"+var);
+		//assign value using =-
+		
+		var -=a; //var =var -a
+		System.out.println("var using -=:"+var);
+		
+
+	}
+
+}
