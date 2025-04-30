ab1a111c0d4655b6056e6b0f30c78a684c6d2640
Ekambaram-Yamuna <yamunaekambaram@gmail.com>
2025-04-30 23:21:30 +0530

    Add files via upload

---
+++ ClassExample.java
@@ -0,0 +1,27 @@
+package Java;
+
+public class ClassExample {
+ int x=5; //variable If we use final keyword it is constant.
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ClassExample obj=new ClassExample(); //obj
+		ClassExample obj1=new ClassExample();
+		ClassExample obj2=new ClassExample();
+		ClassExample obj3=new ClassExample();
+		ClassExample obj4=new ClassExample();
+		System.out.println(obj.x);
+		System.out.println(obj1.x);
+		
+		System.out.println(obj2.x);
+		System.out.println(obj3.x);
+		
+		System.out.println(obj4.x);
+		
+	
+		
+		
+		
+
+	}
+
+}
