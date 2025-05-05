d86811ce69756136b14a7201de1ca2a660fd8071
Ekambaram-Yamuna <yamunaekambaram@gmail.com>
2025-05-05 21:14:03 +0530

    Add files via upload

---
+++ ifelseif.java
@@ -0,0 +1,22 @@
+package Java;
+
+public class ifelseif {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10,b=12, c=18;
+		if(a>b && a>c)
+		{
+			System.out.println("a is greater");
+		}
+		else if(b>a && b>c) {
+			System.out.println("b is greater");
+			
+		}
+		else {
+			System.out.println("c is greater");
+		}
+
+	}
+
+}
