3531b2e7d062bce0683912e40b73aef3507549b4
Ekambaram-Yamuna <yamunaekambaram@gmail.com>
2025-05-05 21:08:25 +0530

    Add files via upload

---
+++ controlStatement.java
@@ -0,0 +1,17 @@
+package Java;
+
+public class controlStatement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=5 ,y=12;
+		if(x+y>20)
+		{
+			System.out.println("The value of x & y is greater than 20");
+		}
+		else {
+			System.out.println("The value is lesser than 20");
+		}
+	}
+
+}
