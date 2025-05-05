8ce19ebd65a9602be67914878b6cf4546b062104
Ekambaram-Yamuna <yamunaekambaram@gmail.com>
2025-05-05 21:11:04 +0530

    Add files via upload

---
+++ if_printnumber.java
@@ -0,0 +1,44 @@
+package Java;
+
+public class if_printnumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i=10;
+		if(i>=1)
+			System.out.println(i);
+		   i--;
+
+	if(i>=1)
+		System.out.println(i--);
+	
+	if(i>=1)
+		System.out.println(i--);
+	
+	if(i>=1)
+		System.out.println(i--);
+	
+	if(i>=1)
+		System.out.println(i--);
+	
+	if(i>=1)
+		System.out.println(i--);
+	
+	if(i>=1)
+		System.out.println(i--);
+	
+	if(i>=1)
+		System.out.println(i--);
+	
+	if(i>=1)
+		System.out.println(i--);
+	
+	if(i>=1)
+		System.out.println(i--);
+	
+	if(i>=1)
+		System.out.println(i--);
+	  
+	}
+
+}
