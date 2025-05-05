fde4fa5353c880123892f97d6f585354f2e50604
Ekambaram-Yamuna <yamunaekambaram@gmail.com>
2025-05-05 21:10:39 +0530

    Add files via upload

---
+++ EncapsulationExample_main.java
@@ -0,0 +1,12 @@
+package Java;
+
+public class EncapsulationExample_main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    EncapsulationExample obj= new EncapsulationExample();
+    obj.setName("Yamuna");
+    System.out.println(obj.getName());
+	}
+
+}
