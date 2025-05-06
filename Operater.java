0bfa3cf684883f68604b1abbe9ab2364cea0f258
Ekambaram-Yamuna <yamunaekambaram@gmail.com>
2025-05-06 15:55:21 +0530

    Add files via upload

---
+++ Operater.java
@@ -0,0 +1,24 @@
+package Java;
+
+public class Operater {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=5;
+		int b=2;
+		int c=a+b;
+		System.out.println("add the values of c"+" "+c);
+		int d=a-b;
+		System.out.println("sub the values of d"+" "+d);
+		int e=a*b;
+		System.out.println("Mul the values of e"+" "+e);
+		int f=a/b;
+		System.out.println("div the values of f"+" "+f);
+		int g=a%b;
+		System.out.println("mod the values of g"+" "+g);
+		
+		
+
+	}
+
+}
