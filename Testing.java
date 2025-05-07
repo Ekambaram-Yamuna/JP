930db6feef6ba668464e6dbc61ec1d911f2089de
Ekambaram-Yamuna <yamunaekambaram@gmail.com>
2025-05-07 19:33:01 +0530

    Add files via upload

---
+++ Testing.java
@@ -0,0 +1,14 @@
+package Java;
+
+public class Testing {
+
+public static void main(String[] args) {
+	// TODO Auto-generated method stub
+	MethodWithParameter obj= new MethodWithParameter();
+	obj.display();
+	obj.speed(250);
+	
+
+}
+
+}
