01db45bd2b131de59340b3ed0e58aa692b71df90
Ekambaram-Yamuna <yamunaekambaram@gmail.com>
2025-05-07 19:28:21 +0530

    Add files via upload

---
+++ switchCase.java
@@ -0,0 +1,35 @@
+package Java;
+
+public class switchCase {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num=3;
+		switch(num) {
+		case 0:
+		{
+			System.out.println("The value is zero");
+		}
+		case 1:
+		{
+			System.out.println("The value is one");
+		}
+		case 2:
+		{
+			System.out.println("The value is two");
+		}
+		case 3:
+		{
+			System.out.println("The values is three");
+		}
+		break;
+		default:
+		{
+			System.out.println(num);
+		}
+		}
+		
+
+	}
+
+}
