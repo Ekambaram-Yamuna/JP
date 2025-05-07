b3e581a325f85062fd6ab026ebed73fd86d9eb16
Ekambaram-Yamuna <yamunaekambaram@gmail.com>
2025-05-07 19:34:09 +0530

    Add files via upload

---
+++ UsingMethod.java
@@ -0,0 +1,21 @@
+package Java;
+
+public class UsingMethod {
+	static void mymethod()
+	{
+		System.out.println("hello SLA");
+	}
+	public void mymethod1()
+	{
+		System.out.println("Hello Summer");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		UsingMethod a=new UsingMethod();
+		a.mymethod();
+		a.mymethod1();
+		
+	}
+
+}
