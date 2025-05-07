f5b94807e3fe60476b6c6e6847b0d502cff5942b
Ekambaram-Yamuna <yamunaekambaram@gmail.com>
2025-05-07 19:27:24 +0530

    Add files via upload

---
+++ SampleProgram.java
@@ -0,0 +1,33 @@
+package Java;
+
+public class SampleProgram {
+
+	public static void main(String[] args) {
+		int a=10;
+		double b=8.5;
+		float c=8.9F;
+		double d=8.2;
+		long e=9737897389L;
+		boolean isfun=true;  //default value of boolean is false
+		char myGrade='a';
+		char myGrade1=65, myGrade2=66,myGrade3=67;
+		String greet="Hello";
+		System.out.println("The value of a is"+" "+a);
+		System.out.println("The value of b is"+" "+b);
+		System.out.println("The value of c is"+" "+c);
+		System.out.println("The value of d is"+" "+d);
+		System.out.println("The value of e is"+" "+e);
+		System.out.println("The value of boolean is"+" "+isfun);
+		System.out.println("The value of char is"+" "+myGrade);
+		System.out.println("the values of mygrade1 is"+" "+myGrade1);
+		System.out.println("the values of mygrade2 is"+" "+myGrade2);
+		System.out.println("the values of mygrade3 is"+" "+myGrade3);
+		System.out.println("The values of string is"+" "+greet);
+		
+		
+		
+		
+
+	}
+
+}
