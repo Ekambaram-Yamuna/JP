deeefe63c3e8c7f43f9747a82a74106df15b74e4
Ekambaram-Yamuna <yamunaekambaram@gmail.com>
2025-05-07 19:32:40 +0530

    Add files via upload

---
+++ TernaryOperator.java
@@ -0,0 +1,15 @@
+package Java;
+
+public class TernaryOperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num1=10;
+		int num2=20;
+		//using the ternary operator to find the maximum of two  numbers.
+		int max=(num1 > num2 )? num1:num2;
+		System.out.println("The maximum number is :"+max);
+
+	}
+
+}
